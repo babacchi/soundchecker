3cb5b12bcad82da7bf911c35dd5ed4c955d39a5a
Kazunori Baba <babacchi@gmail.com>
2026-05-19 15:04:30 +0900

    next.confiを本番環境で使用できるように修正

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
 import type { NextConfig } from "next";
-
+const isProd = process.env.NODE_ENV === "production";
+const repositoryName = "soundchecker";
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: '/soundchecker',
   output: "export",
+  ...(isProd
+    ? {
+        basePath: `/${repositoryName}`,
+        assetPrefix: `/${repositoryName}/`,
+      }
+    : {}),
 };
 
 export default nextConfig;
